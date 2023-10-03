3fdc1894bc432482ea7cecc7675ee9db2ee57db1
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-03 16:53:48 -0700

    update recipe model

---
+++ models/recipe.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    servings: Number,
+    steps: [{type: String, required: true}],
+    ingredients: [
+      {
+        ingredient: {type: String, required: true},
+        unit: {type: Number, required: true},
+        amount: {type: Number, required: true},
+      },
+    ],
+    time: {hours: {type: Number}, minutes: {type: Number}},
+    userId: {
+      type: String,
+      required: true,
+    },
+    isRecommended: {type: Map, of: Boolean},
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: "Comments"}],
+    review: [{type: mongoose.Schema.Types.ObjectId, ref: "Review"}],
+    tags: {type: Array, default: []},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Recipe", RecipeSchema);
