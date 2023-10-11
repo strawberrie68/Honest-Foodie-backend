2bfca4f3c74507c48ba6b229b83aecb96ca79e1b
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-11 07:01:04 -0700

    auth and login

--- utils/middleware.js
+++ utils/middleware.js
@@ -16,7 +16,37 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+const tokenExtractor = (request, response, next) => {
+  request.token = getTokenFrom(request);
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const token = getTokenFrom(request);
+
+  if (token) {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({error: "token invalid"});
+    }
+
+    request.user = await User.findById(decodedToken.id);
+  }
+
+  next();
+};
+
 module.exports = {
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 };
