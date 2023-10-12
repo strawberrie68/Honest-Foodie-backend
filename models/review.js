202604ce545dcc0d7836c21afe7214e991768db1
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-12 10:04:42 -0700

    review model created

---
+++ models/review.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = mongoose.Schema({
+  recipeId: {
+    type: String,
+    required: true,
+  },
+  reviewer: {
+    type: String,
+    required: true,
+  },
+  timesMade: {type: Number, default: 1},
+  rating: Number,
+  picturePath: String,
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
