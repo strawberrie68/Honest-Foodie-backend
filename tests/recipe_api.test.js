210712673930d02045d76943fd1a39d7f880bcb8
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-14 10:46:09 -0700

    cleaned recipe tests

--- tests/recipe_api.test.js
+++ tests/recipe_api.test.js
@@ -61,7 +61,6 @@ describe("recipes api", () => {
       await api.post("/api/recipe/add").send(recipe);
 
       const newResponse = await api.get(`/api/recipe/${userID}/recipes`);
-      console.log(newResponse.body);
 
       expect(recipe.title).toContain("strawberry shortcake");
     });
@@ -78,41 +77,7 @@ describe("recipes api", () => {
 
     describe("a new recipe", () => {
       test("can be added", async () => {
-        const recipe = {
-          title:
-            "strawberry shortcake new version - check if recipe can be added",
-          picturePath:
-            "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.ricardocuisine.com%2Fservices%2Frecipes%2F9640.jpg&tbnid=eFe18pfPtTruUM&vet=12ahUKEwjbw9ybiO2BAxWSBzQIHd8WAyoQMygOegUIARCWAQ..i&imgrefurl=https%3A%2F%2Fwww.ricardocuisine.com%2Fen%2Frecipes%2F9640-japanese-strawberry-shortcake&docid=K_d7KXa2YhtFcM&w=1920&h=2592&q=strawberry%20shortcake%20japanese&ved=2ahUKEwjbw9ybiO2BAxWSBzQIHd8WAyoQMygOegUIARCWAQ",
-          servings: "6",
-          steps: [
-            {
-              stepName: "Cake",
-              step: [
-                "In a large bowl, whisk the egg whites with an electric mixer until soft peaks form. Gradually whisk in the sugar until semi-stiff peaks form.",
-                "In another bowl, whisk the egg yolks with the oil and milk. Add the flour. Add one-quarter of the meringue mixture and stir to combine. Using a spatula, gently fold in the remaining meringue. Spread the batter out in the two prepared cake pans.",
-                "Bake for 20 minutes or until a toothpick inserted in the centre of a cake comes out clean. Remove from the oven and immediately turn the cake pans over on a wire rack to cool completely, about 2 hours. Run a thin blade between the side of the pans and the cakes to unmould.",
-              ],
-            },
-          ],
-          ingredients: [
-            {
-              ingredientsFor: "Cake",
-              allIngredients: [
-                {
-                  ingredient: "eggs",
-                  unit: "none",
-                  amount: 3,
-                },
-              ],
-            },
-          ],
-          time: {hours: "1", minutes: "30"},
-          urserId: "498573895",
-          isRecommended: {},
-          comments: [],
-          review: [],
-          tags: ["cake", "strawberry"],
-        };
+        const recipe = initialRecipes[0];
 
         await api
           .post("/api/recipe/add")
