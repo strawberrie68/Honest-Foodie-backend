e7a8326968394ef4682c26a7343cadd8b31ae8c2
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-22 21:46:48 -0700

    tests updated

--- tests/recipe_api.test.js
+++ tests/recipe_api.test.js
@@ -5,7 +5,7 @@ const {
   initialRecipes,
   initialUsers,
   recipesInDb,
-  usersInDb,
+  recipeToAdd,
 } = require("./test_helper");
 
 const api = supertest(app);
@@ -13,7 +13,10 @@ const Recipe = require("../models/Recipe");
 const User = require("../models/User");
 
 let authHeader;
-describe("recipes api", () => {
+let userId;
+let recipeId;
+
+describe("2.recipe api", () => {
   beforeEach(async () => {
     await User.deleteMany({});
 
@@ -21,98 +24,100 @@ describe("recipes api", () => {
     const user = initialUsers[0];
     await api.post("/api/users").send(user);
     const response = await api.post("/api/auth/login").send(user);
-
+    userId = response.body.id;
     authHeader = `Bearer ${response.body.token}`;
   });
 
-  beforeEach(async () => {
-    await Recipe.deleteMany({});
-    await Recipe.insertMany(initialRecipes);
-  });
+  describe("add new recipe", () => {
+    test("can be added", async () => {
+      await Recipe.deleteMany({});
+      const recipe = recipeToAdd;
+      const beforeRecipes = await recipesInDb();
 
-  describe("BASIC FETCH - when there is initially some recipe saved", () => {
-    test("recipe are returned as json", async () => {
-      await api
-        .get("/api/recipe")
-        .expect(200)
+      const response = await api
+        .post("/api/recipe/create")
+        .set("Authorization", authHeader)
+        .send(recipe)
+        .expect(201)
         .expect("Content-Type", /application\/json/);
+
+      recipeId = response.body._id;
+
+      const afterRecipes = await recipesInDb();
+
+      expect(afterRecipes).toHaveLength(beforeRecipes.length + 1);
     });
 
-    test("all recipes are returned", async () => {
-      const response = await api.get("/api/recipe");
+    test("can be updated", async () => {
+      const recipeBefore = await recipesInDb();
 
-      expect(response.body).toHaveLength(initialRecipes.length);
-    });
+      const modifiedRecipe = {
+        ...recipeBefore[0],
+        title: "Modified Recipe Title",
+      };
 
-    test("a specific recipe is within the returned recipe", async () => {
-      const response = await api.get("/api/recipe");
+      await api
+        .patch(`/api/recipe/${recipeId}/edit`)
+        .send(modifiedRecipe)
+        .expect(200);
 
-      const titles = response.body.map((r) => r.title);
-
-      expect(titles).toContain("strawberry shortcake");
-    });
-
-    test("able to return user's recipe using userId", async () => {
-      const user = await usersInDb();
-      const userID = user.map((r) => r.id);
-      const recipe = initialRecipes[0];
-
-      await api.post("/api/recipe/add").send(recipe);
-      const newResponse = await api.get(`/api/recipe/${userID}/recipes`);
-
-      expect(newResponse.title).toContain("strawberry shortcake");
-    });
-
-    test("able to get a specfic recipe using recipeId", async () => {
       const recipes = await recipesInDb();
-      const recipeId = recipes[0]._id.toString();
-
-      const response = await api.get(`/api/recipe/${recipeId}`);
-
-      expect(response.body.title).toContain("strawberry shortcake");
+      const titles = recipes[0].title;
+      expect(titles).toContain(modifiedRecipe.title);
     });
 
-    describe("a new recipe", () => {
-      test("can be added", async () => {
+    describe("recipe can be", () => {
+      beforeEach(async () => {
         const recipe = initialRecipes[0];
 
-        await api
-          .post("/api/recipe/add")
+        const response = await api
+          .post("/api/recipe/create")
           .set("Authorization", authHeader)
-          .send(recipe)
-          .expect(201)
-          .expect("Content-Type", /application\/json/);
+          .send(recipe);
 
-        const recipes = await recipesInDb();
-
-        expect(recipes).toHaveLength(initialRecipes.length + 1);
-
-        const titles = recipes.map((r) => r.title);
-
-        expect(titles).toContain(recipe.title);
+        recipeId = response.body._id;
       });
 
-      test("can not be added when theres missing info", async () => {
-        const recipe = {
-          title:
-            "strawberry shortcake new version - check if recipe can be added",
-        };
+      test("deleted by the creator", async () => {
+        const recipeBefore = await recipesInDb();
 
         await api
-          .post("/api/recipe/add")
+          .delete(`/api/recipe/${recipeId}/delete`)
           .set("Authorization", authHeader)
-          .send(recipe)
-          .expect(400);
+          .expect(204);
+        const recipesAfter = await recipesInDb();
 
-        const recipes = await recipesInDb();
-        expect(recipes).toHaveLength(initialRecipes.length);
+        expect(recipesAfter).toHaveLength(recipeBefore.length - 1);
+      });
+
+      test("can not be deleted without valid auth header", async () => {
+        const recipeBefore = await recipesInDb();
+
+        await api.delete(`/api/recipe/${recipeId}/delete`).expect(401);
+
+        const recipesAfter = await recipesInDb();
+
+        expect(recipesAfter).toHaveLength(recipeBefore.length);
+      });
+
+      test("able to return user's recipe using userId", async () => {
+        const newResponse = await api.get(`/api/recipe/${userId}/recipes`);
+
+        expect(newResponse.body.recipes[0].title).toContain(
+          "strawberry shortcake"
+        );
+      });
+
+      test("able to return recipe using recipeId", async () => {
+        await api
+          .get(`/api/recipe/${recipeId}`)
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
       });
     });
-
     describe("a new review", () => {
       test("can be added", async () => {
-        const response = await api.get("/api/recipe");
-        const ids = response.body.map((r) => r._id);
+        const beforeAddingReview = await api.get(`/api/recipe/${recipeId}`);
 
         const review = {
           timesMade: 2,
@@ -121,25 +126,45 @@ describe("recipes api", () => {
         };
 
         await api
-          .post(`/api/recipe/${ids[0]}`)
+          .post(`/api/recipe/${recipeId}`)
           .set("Authorization", authHeader)
           .send(review)
           .expect(201)
           .expect("Content-Type", /application\/json/);
 
-        const recipes = await recipesInDb();
+        const afterAddingReview = await api.get(`/api/recipe/${recipeId}`);
 
-        expect(recipes[0].review).toHaveLength(
-          initialRecipes[0].review.length + 1
+        expect(afterAddingReview.body.review).toHaveLength(
+          beforeAddingReview.body.review.length + 1
+        );
+      });
+
+      test("is added to user's review", async () => {
+        const beforeAddingReview = await api.get(`/api/users/${userId}`);
+
+        const review = {
+          timesMade: 2,
+          rating: 5,
+          picturePath: "thisIsPicture",
+        };
+
+        await api
+          .post(`/api/recipe/${recipeId}`)
+          .set("Authorization", authHeader)
+          .send(review)
+          .expect(201)
+          .expect("Content-Type", /application\/json/);
+
+        const afterAddingReview = await api.get(`/api/users/${userId}`);
+
+        expect(afterAddingReview.body.reviews).toHaveLength(
+          beforeAddingReview.body.reviews.length + 1
         );
       });
     });
 
     describe("a new comment", () => {
       test("can be added", async () => {
-        const response = await api.get("/api/recipe");
-        const recipeId = response.body[0]._id;
-
         const beforeUpdate = await api.get(`/api/recipe/${recipeId}`);
 
         const comment = {
@@ -164,9 +189,6 @@ describe("recipes api", () => {
       });
 
       test("can not be added when missing info", async () => {
-        const response = await api.get("/api/recipe");
-        const recipeId = response.body[0]._id;
-
         const comment = {
           parentId: null,
         };
@@ -180,8 +202,8 @@ describe("recipes api", () => {
       });
     });
   });
+});
 
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
+afterAll(async () => {
+  await mongoose.connection.close();
 });
