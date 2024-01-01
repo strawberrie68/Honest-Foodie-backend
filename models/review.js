f6bd7dcc9ebfd3067d006b3aa6c0320f6e0b4a18
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-12-31 21:32:46 -0800

    review model created

--- models/review.js
+++ models/review.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = mongoose.Schema({
-  recipeId: {
-    type: String,
-    required: true,
-  },
-  reviewer: {
-    type: String,
-    required: true,
-  },
-  timesMade: {type: Number, default: 1},
+  recipeId: { type: mongoose.Schema.Types.ObjectId, ref: "Recipe" },
+  reviewer: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  timesMade: { type: Number, default: 1 },
   rating: Number,
   picturePath: String,
+  userReview: String,
+  isRecommended: Boolean,
 });
 
 const Review = mongoose.model("Review", reviewSchema);
