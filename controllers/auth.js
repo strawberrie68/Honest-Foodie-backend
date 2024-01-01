090a671b2c1f9c1d0a5d287a55697f930df0f6e9
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-12-31 21:15:19 -0800

    added auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,27 +4,59 @@ const User = require("../models/User");
 
 module.exports = {
   login: async (request, response) => {
-    const {username, password} = request.body;
+    try {
+      const { username, password } = request.body;
+      const user = await User.findOne({ username });
+      console.log(user);
+      if (!user)
+        return response.status(400).json({ error: "User does not exist." });
 
-    const user = await User.findOne({username});
-    const passwordCorrect =
-      user === null ? false : await bcrypt.compare(password, user.passwordHash);
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return response.status(400).json({ error: "Invalid credentials." });
 
-    if (!(user && passwordCorrect)) {
-      return res.status(401).json({
-        error: "invalid username or password",
-      });
+      const userToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      const token = jwt.sign(userToken, process.env.SECRET);
+      delete user.password;
+      response
+        .status(200)
+        .json({ token, username: user.username, name: user.firstName, user });
+    } catch (err) {
+      response.status(500).json({ error: err.message });
     }
+    // try {
+    //   const { username, password } = request.body;
 
-    const userForToken = {
-      username: user.username,
-      id: user._id,
-    };
+    //   const user = await User.findOne({ username });
+    //   if (!user) {
+    //     return response.status(401).json({
+    //       error: "invalid username or password",
+    //     });
+    //   }
 
-    const token = jwt.sign(userForToken, process.env.SECRET);
+    //   const passwordCorrect = await bcrypt.compare(password, user.passwordHash);
+    //   if (!passwordCorrect) {
+    //     return response.status(401).json({
+    //       error: "invalid username or password",
+    //     });
+    //   }
 
-    response
-      .status(200)
-      .send({token, username: user.username, name: user.name});
+    //   const userToken = {
+    //     username: user.username,
+    //     id: user._id,
+    //   };
+
+    //   const token = jwt.sign(userToken, process.env.SECRET);
+
+    //   response
+    //     .status(200)
+    //     .send({ token, username: user.username, name: user.name });
+    // } catch (err) {
+    //   response.status(500).json({ error: err.message });
+    // }
   },
 };
