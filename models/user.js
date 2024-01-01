1d0893f5bc18b6e59e6c000e5b92966c43ffe880
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-12-31 21:30:54 -0800

    updated user model and routes

--- models/user.js
+++ models/user.js
@@ -16,18 +16,20 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  following: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-  followers: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-  email: {type: String, required: true},
-  passwordHash: String,
+  // following: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+  // followers: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+  following: { type: Map, of: Boolean },
+  followers: { type: Map, of: Boolean },
+  email: { type: String, required: true },
+  password: String,
   picturePath: {
     type: String,
     default: "",
   },
-  recipes: [{type: mongoose.Schema.Types.ObjectId, ref: "Recipe"}],
-  savedRecipe: [{type: mongoose.Schema.Types.ObjectId, ref: "Recipe"}],
-  reviews: [{type: mongoose.Schema.Types.ObjectId, ref: "Review"}],
-  flavorProfile: {type: Array, default: []},
+  recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
+  savedRecipe: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+  flavorProfile: { type: Array, default: [] },
   caption: String,
 });
 
@@ -39,7 +41,7 @@ userSchema.set("toJSON", {
     delete returnedObject._id;
     delete returnedObject.__v;
     // the passwordHash should not be revealed
-    delete returnedObject.passwordHash;
+    delete returnedObject.password;
   },
 });
 
