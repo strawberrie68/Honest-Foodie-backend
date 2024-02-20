adea13c06a8b27ff50d2355cb9d8d4d9c678a1a2
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-19 23:18:35 -0800

    refactor: read test

--- tests/recipe/READ_recipes.test.js
+++ tests/recipe/READ_recipes.test.js
@@ -8,33 +8,22 @@ const mongoose = require("mongoose");
 const { initialRecipes, initialUsers } = require("../test_helper");
 const STATUS_CODE = require("../../shared/errorCode");
 
-// User related variables
-let authHeader;
-let userId;
-let token;
-
-// Recipe related variables
-let recipeId;
-let testRecipe;
-
 const RECIPE_API = "/api/recipe";
+const RECIPES = "recipes";
 
-const { createUserAndLogin, addRecipe } = require("../api_test_helper");
-
-const setup = async () => {
-  await User.deleteMany({});
-  const newUser = initialUsers[1];
-  let response = await createUserAndLogin(newUser);
-
-  token = response.body.token;
-  authHeader = `Bearer ${token}`;
-  userId = response.body.id;
-  testRecipe = initialRecipes[0];
-};
+const { createUserAndLogin, addRecipe } = require("../api_test_helpers");
 
 describe("READ - recipes", () => {
+  let token, authHeader, userId, recipeId, testRecipe;
   beforeEach(async () => {
-    await setup();
+    await User.deleteMany({});
+    const newUser = initialUsers[1];
+    let response = await createUserAndLogin(newUser);
+
+    token = response.body.token;
+    authHeader = `Bearer ${token}`;
+    userId = response.body.id;
+    testRecipe = initialRecipes[0];
   });
 
   describe("get all recipes from the database", () => {
@@ -44,7 +33,7 @@ describe("READ - recipes", () => {
     });
 
     test("should return all recipes", async () => {
-      const response = await api.get(RECIPE_API);
+      const response = await api.get(RECIPE_API).expect(STATUS_CODE.OK);
 
       expect(response.body).toHaveLength(initialRecipes.length);
     });
@@ -57,7 +46,9 @@ describe("READ - recipes", () => {
     });
 
     test("should return all recipes of a user when given a userId", async () => {
-      const response = await api.get(`${RECIPE_API}/${userId}/recipes`);
+      const response = await api
+        .get(`${RECIPE_API}/${userId}/${RECIPES}`)
+        .expect(STATUS_CODE.OK);
 
       expect(response.body.recipes[0].title).toContain(testRecipe.title);
       expect(response.body.recipes).toHaveLength(1);
