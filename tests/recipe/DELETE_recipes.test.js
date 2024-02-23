2e535fcf21d09dd848030d5b587f789d743c6e35
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-22 20:45:07 -0800

    refactor: delete test

--- tests/recipe/DELETE_recipes.test.js
+++ tests/recipe/DELETE_recipes.test.js
@@ -10,32 +10,35 @@ const STATUS_CODE = require("../../shared/errorCode");
 
 const { createUserAndLogin, addRecipe } = require("../api_test_helpers.js");
 
-const RECIPE_API = "api/recipe";
-const DELETE = "delete";
+const RECIPE_API = "/api/recipe";
+const DELETE = "/delete";
+
+const setupTest = async () => {
+  const newUser = initialUsers[1];
+  const recipe = initialRecipes[0];
+
+  const response = await createUserAndLogin(newUser);
+  const token = response.body.token;
+  const authHeader = `Bearer ${token}`;
+  const recipeResponse = await addRecipe(recipe, authHeader);
+  const recipeId = recipeResponse.body._id;
+
+  return { token, recipeId };
+};
 
 describe("recipe api", () => {
-  let token, authHeader, userId, recipeId;
   beforeEach(async () => {
     await User.deleteMany({});
     await Recipe.deleteMany({});
-    const newUser = initialUsers[1];
-    const recipe = initialRecipes[0];
-    let response = await createUserAndLogin(newUser);
-
-    token = response.body.token;
-    authHeader = `Bearer ${token}`;
-    userId = response.body.id;
-
-    const recipeResponse = await addRecipe(recipe, authHeader);
-    recipeId = recipeResponse.body._id;
   });
 
   test("should not delete a recipe when the authorization header is invalid", async () => {
+    const { recipeId } = await setupTest();
     let recipes = await recipesInDb();
     expect(recipes).toHaveLength(1);
 
     await api
-      .delete(`/${RECIPE_API}/${recipeId}/${DELETE}`)
+      .delete(`${RECIPE_API}/${recipeId}${DELETE}`)
       .expect(STATUS_CODE.NOT_AUTHORIZED);
     recipes = await recipesInDb();
 
@@ -44,11 +47,12 @@ describe("recipe api", () => {
   });
 
   test("should delete a recipe if the user is the creator", async () => {
+    const { token, recipeId } = await setupTest();
     let recipes = await recipesInDb();
     expect(recipes).toHaveLength(1);
 
     await api
-      .delete(`/${RECIPE_API}/${recipeId}/${DELETE}`)
+      .delete(`${RECIPE_API}/${recipeId}${DELETE}`)
       .set("Authorization", `Bearer ${token}`)
       .expect(STATUS_CODE.NO_CONTENT);
     recipes = await recipesInDb();
