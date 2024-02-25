694085549e9a50f862275fa75c76e6d4cea82cb1
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-25 10:28:09 -0800

    refactor: update recipes test

--- tests/recipe/UPDATE_recipes.test.js
+++ tests/recipe/UPDATE_recipes.test.js
@@ -20,6 +20,22 @@ const INVALID_RECIPE_ID = "1234567890";
 const MODIFIED_TITLE = "This title is a modified recipe title";
 const UNMODIFIED_TITLE = "This title should not be updated";
 
+const setupTest = async () => {
+  await User.deleteMany({});
+  await Recipe.deleteMany({});
+  const newUser = initialUsers[1];
+  const recipe = initialRecipes[0];
+
+  const response = await createUserAndLogin(newUser);
+  const token = response.body.token;
+  const authHeader = `Bearer ${token}`;
+  const userId = response.body.id;
+
+  const recipeResponse = await addRecipe(recipe, authHeader);
+  const recipeId = recipeResponse.body._id;
+  return { token, authHeader, userId, recipeId };
+};
+
 const getRecipesAndTitles = async () => {
   const recipes = await recipesInDb();
   const recipeTitles = recipes.map((recipe) => recipe.title);
@@ -27,24 +43,9 @@ const getRecipesAndTitles = async () => {
 };
 
 describe("recipe api", () => {
-  let authHeader, token, recipeId, userId;
-  beforeEach(async () => {
-    await User.deleteMany({});
-    await Recipe.deleteMany({});
-    const newUser = initialUsers[1];
-    const recipe = initialRecipes[0];
-
-    let response = await createUserAndLogin(newUser);
-    token = response.body.token;
-    authHeader = `Bearer ${token}`;
-    userId = response.body.id;
-
-    const recipeResponse = await addRecipe(recipe, authHeader);
-    recipeId = recipeResponse.body._id;
-  });
-
   describe("UPDATE - recipes", () => {
     test("should UPDATE a recipe when given a VALID recipeId", async () => {
+      const { token, recipeId } = await setupTest();
       let { recipes, recipeTitles } = await getRecipesAndTitles();
       expect(recipes).toHaveLength(1);
 
@@ -63,6 +64,7 @@ describe("recipe api", () => {
     });
 
     test("should NOT update a recipe when given a INVALID recipeId", async () => {
+      const { token } = await setupTest();
       let { recipes, recipeTitles } = await getRecipesAndTitles();
       expect(recipes).toHaveLength(1);
 
